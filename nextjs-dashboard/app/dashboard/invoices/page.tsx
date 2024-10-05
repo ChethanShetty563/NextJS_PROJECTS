0220986694161bd2b15dc9e17e69e2df7ea00196
ChethanShetty563 <chethan563@gmail.com>
2024-10-05 18:37:59 +0530

    Adding the create page

--- nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -7,14 +7,14 @@ import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default function Page({searchParams,} : {searchParams?: {
+export default async function  Page({searchParams,} : {searchParams?: {
     query?: string;
     page?: string;
 }
 }) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
-    const totalPages = fetchInvoicesPages(query);
+    const totalPages =await fetchInvoicesPages(query);
     return <div className="w-full">
         <div className="flex w-full items-center justify-between">
             <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
